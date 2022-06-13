cb4dbc8bf968c701ddf573292f212d6f886f2e5d
Andrea leopardi <an.leopardi@gmail.com>
2022-06-13 15:14:46 +0200

    Better README and docs

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # NimbleLZ4
 
-> Proof-of-concept lz4 compression library for Elixir that uses Rust NIFs.
+> [LZ4][lz4] compression library for Elixir that uses Rust NIFs.
 
-⚠️ **Do not use**. This is not built for production in any way. ⚠️
+## Installation
 
-I built this to run some tests with [Xandra].
+Add this to your dependencies in `mix.exs`.
 
-[Xandra]: https://github.com/lexhide/xandra
+```elixir
+defp deps do
+  [
+    # ...,
+    {:nimble_lz4, "~> 0.1.0"}
+  ]
+end
+```
+
+## Usage
+
+You can compress and decompress data.
+
+```elixir
+iex> uncompressed = :crypto.strong_rand_bytes(10)
+iex> compressed = NimbleLZ4.compress(uncomppressed)
+iex> uncompressed == NimbleLZ4.decompress(compressed, _uncompressed_size = 10)
+true
+```
+
+[lz4]: https://github.com/lz4/lz4
--- lib/nimble_lz4.ex
+++ lib/nimble_lz4.ex
@@ -1,15 +1,40 @@
 defmodule NimbleLZ4 do
   @moduledoc """
-  TODO
+  [LZ4](https://github.com/lz4/lz4) compression and decompression.
+
+  This functionality is built on top of native (Rust) NIFs. There is no
+  streaming functionality, everything is done in memory.
+
+  ## Uncompressed Size
+
+  `decompress/2` takes the original uncompressed size as a parameter. For this
+  reason, it's common to store compressed binaries *prefixed by their uncompressed
+  length*. For example, you could store the compressed binary as:
+
+      my_binary = <<...>>
+      store(<<byte_size(my_binary)::32>> <> NimbleLZ4.compress(my_binary))
+
+  When decompressing, you can extract the uncompressed length:
+
+      <<uncompressed_size::32, compressed_binary::binary>> = retrieve_binary()
+      uncompressed_binary = NimbleLZ4.decompress(compressed_binary, uncompressed_size)
+
   """
 
   use Rustler, otp_app: :nimble_lz4, crate: "nimblelz4"
 
+  @doc """
+  Compresses the given binary.
+  """
+  @doc since: "0.1.0"
   @spec compress(binary()) :: binary()
   def compress(_binary) do
     :erlang.nif_error(:nif_not_loaded)
   end
 
+  @doc """
+  Decompresses the given binary using the size of the uncompressed binary.
+  """
   @spec decompress(binary(), non_neg_integer()) :: {:ok, binary()} | {:error, term()}
   def decompress(_binary, _uncompressed_size) do
     :erlang.nif_error(:nif_not_loaded)
